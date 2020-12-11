3ced63617f578eea074d3dd9ad8123232c17906f
Piotr Kaminski <kaminski.piotr@gmail.com>
2020-12-11 19:30:47 +0100

    Introduce script to notify current state of points controlled by pshActor - create notify-current-state.sh script - update messages for reset-all-points.sh

--- psh-actor/notify-current-status.sh
+++ psh-actor/notify-current-status.sh
@@ -5,7 +5,7 @@ echo "Getting Actor name from configuration to identify mqtt topic name..."
 CLIENT_NAME=`/usr/bin/python3 ./configuration.py`
 
 TOPIC="/$CLIENT_NAME/In/Admin"
-COMMAND="RESET"
+COMMAND="NOTIFY_CURRENT_STATE"
 
 echo "Sending $COMMAND command to mqtt topic $TOPIC..."
 
